86d863b4cd4e79f00918e86cb271c292d3a95c88
Akshat Pandey <aplinkinpark16@gmail.com>
2024-01-26 15:07:25 +0530

    about edited

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -15,7 +15,7 @@ const About: React.FC = () => {
         </p>
       </div>
       <div className='flex flex-box justify-center items-center absolute top-24 left-32 mr-24 max-w-[900px]'>
-        <p className="text-white text-2xl mt-14">
+        <p className="text-white text-2xl mt-8">
           Lorem ipsum dolor sit amet cons... Got you. I am a 22 year old who is currently pursuing his Bachelors in Computer Science and Engineering. I am a self + teacher taught developer who is always looking for new opportunities to learn and grow. I am a quick learner and a team player and other cherry picked words. 
           I am probably a good developer and not so good designer. I have been involved in various projects and hackathons, winning one of these, and leading in many, to give a  hint of leadership to the reader. I dont shy away to make use of AI tools to take help and accomplish task. I hope this single page website would be a testement to my skills and abilities. Enjoy your stay here!
           p.s. We can talk about my wierd font choice and color scheme in the interviews.
