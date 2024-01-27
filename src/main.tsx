34615c893b8aeab4c9ae7a4da83af82bbd59423d
Akshat Pandey <aplinkinpark16@gmail.com>
2024-01-27 20:14:50 +0530

    updated main.tsx and about

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Home from './pages/Home'
-import About from './pages/About'
-import Internships from './pages/Internships'
-import Projects from './pages/Projects'
-import Skills from './pages/Skills'
-import Hire from './pages/Hire'
-import Contact from './pages/Contact'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Internships from './pages/Internships';
+import Projects from './pages/Projects';
+import Skills from './pages/Skills';
+import Hire from './pages/Hire';
+import Contact from './pages/Contact';
 
+const App: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <Home />
+      <About />
+      <Internships />
+      <Projects />
+      <Skills />
+      <Hire />
+      <Contact />
+    </React.StrictMode>
+  );
+};
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Home />
-    <About />
-    <Internships />
-    <Projects />
-    <Skills />
-    <Hire />
-    <Contact />
-  </React.StrictMode>,
-)
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -14,7 +14,7 @@ const About: React.FC = () => {
           See what I cherry picked about myself!
         </p>
       </div>
-      <div className='z-50 flex flex-box justify-center items-center rounded-2xl bg-emerald-800 border-emerald-300 border-2 pr-10 pl-10 pb-10 absolute top-20 bg-opacity-90 left-28 max-w-[800px]'>
+      <div className='z-50 flex flex-box justify-center items-center rounded-2xl bg-emerald-800 border-emerald-300 border-2 pr-10 pl-10 pb-10 absolute top-20 bg-opacity-90 left-20 max-w-[800px]'>
         <p className="text-white text-2xl mt-8 text-justify">
           Lorem ipsum dolor sit amet cons... Got you. I am a 22 year old who is currently pursuing his Bachelors in Computer Science and Engineering at Bhilai Institute of Technology, Durg. I am a self + teacher taught developer who is always looking for new opportunities to learn and grow. I am a quick learner and a team player and other cherry picked words. 
           I am probably a good developer and not so good designer. I have been involved in various projects and hackathons, winning one of these, and leading in many, to give a  hint of leadership to the reader. I dont shy away to make use of AI tools to take help and accomplish task. <br/><br/>I hope this single page website would be a testement to my skills and abilities. Enjoy your stay here!<br/> <br/>
